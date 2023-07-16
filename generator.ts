43509d086d4779a456fa007533c0cb3f1df62a97
Microsvuln <55649192+Microsvuln@users.noreply.github.com>
2023-07-16 15:17:26 +0200

    Add files via upload

---
+++ generator.ts
@@ -0,0 +1,38 @@
+import * as fdIr from "./FD-IR";
+import GlobalContext from './GlobalContext';
+
+
+// First, create a global context. This could be as simple as an object literal,
+// but in real use, you might want to include more complex state.
+
+
+
+let ctx = new GlobalContext();
+
+let id = 0; // or whatever initial value you want
+let name = 'someName'; // replace with actual value
+let value = 'someValue'; // replace with actual value
+
+
+
+// Instantiate an object for each class in FD-IR
+//// const instances = Object.values(fdIr).map(FdIrClass => new FdIrClass());
+const instances = Object.values(fdIr).map(FdIrClass => new FdIrClass(id, name, value));  // assuming `id`, `name`, `value` are defined.
+
+instances.forEach(instance => instance.generate(ctx));
+
+// Run the generate method for each instance
+//// instances.forEach(instance => instance.generate(ctx));
+
+// Then you can run a number of iterations where you randomly select an instance
+// and run the mutate method on it.
+for (let i = 0; i < 100; i++) {
+  const instance = instances[Math.floor(Math.random() * instances.length)];
+  instance.mutate(ctx);
+}
+
+// Finally, use the lower method to get a string representation of the code
+const loweredCode = instances.map(instance => instance.lower()).join("\n");
+
+// You can then write the lowered code to a file, or execute it, or use it however you need.
+console.log(loweredCode);
---
+++ idl2ir3.js
@@ -0,0 +1,127 @@
+const webidl2 = require("webidl2");
+const fs = require("fs");
+
+const webAudioWebIDL = fs.readFileSync('./WebAudio.idl', 'utf-8');
+const parsedWebIDL = webidl2.parse(webAudioWebIDL);
+
+function generateValue(type) {
+    switch (type) {
+      case 'unsigned long':
+        return 1;  // Generate a default unsigned long value
+      case 'float':
+        return 1.0;  // Generate a default float value
+      case 'AudioContextOptions':
+      case 'OfflineAudioContextOptions':
+      case 'AudioBufferOptions':
+      case 'AnalyserOptions':
+      case 'AudioBufferSourceOptions':
+      case 'OfflineAudioCompletionEventInit':
+      case 'AudioProcessingEventInit':
+      case 'BiquadFilterOptions':
+      case 'ChannelMergerOptions':
+      case 'ChannelSplitterOptions':
+      case 'ConstantSourceOptions' :
+      case 'ConvolverOptions' :
+      case 'DelayOptions' :            
+      case 'DynamicsCompressorOptions' :            
+      case 'GainOptions' :            
+      case 'IIRFilterOptions' :            
+      case 'AudioContext' :            
+      case 'MediaElementAudioSourceOptions' :            
+      case 'AudioNodeOptions' :
+      case 'MediaStreamAudioSourceOptions' :
+      case 'MediaStreamTrackAudioSourceOptions' :
+      case 'OscillatorOptions' :
+      case 'PannerOptions' :
+      case 'PeriodicWaveOptions' :
+      case 'StereoPannerOptions' :
+      case 'WaveShaperOptions' :
+      case 'AudioWorkletNodeOptions' :
+      case 'MediaStreamAudioSourceOptions' :
+      case 'MediaStreamAudioSourceOptions' :
+
+
+
+
+
+
+
+        return '{}';  // Generate a default empty object for dictionary types
+      case 'DOMString':
+        return '"default"';  // Generate a default string value
+      case 'BaseAudioContext':
+        return 'new AudioContext()';  // Generate a new AudioContext instance
+      // Add cases for other types as needed
+      default:
+        throw new Error(`Don't know how to generate a value of type ${type}`);
+    }
+  }
+  
+  
+  
+  
+  
+  
+  
+
+function generateFDIR(parsedWebIDL) {
+  let fdirSource = '';
+
+  fdirSource += `import { Value } from './Value';\n`;
+  fdirSource += `import GlobalContext from './GlobalContext';\n\n`;
+
+  for (const item of parsedWebIDL) {
+    if (item.type === 'interface') {
+      const className = item.name;
+      const idlName = item.name.replace(/([A-Z])/g, '-$1').toLowerCase();
+
+      const constructors = item.members.filter(member => member.type === "constructor");
+
+      fdirSource += `class ${className}Value extends Value {\n`;
+      fdirSource += `  private _type: string = "${idlName}";\n`;
+      fdirSource += `  private _id: number;\n`;
+      fdirSource += `  private _parameters: any[];\n\n`;
+      fdirSource += `  generate(ctx: GlobalContext): void {\n`;
+      fdirSource += `    this._id = ctx.create${className}Id();\n`;
+
+      if (constructors.length > 0) {
+        // Choose a random constructor
+        fdirSource += `    const constructorIndex = Math.floor(Math.random() * ${constructors.length});\n`;
+        fdirSource += `    switch (constructorIndex) {\n`;
+
+        constructors.forEach((constructor, index) => {
+          fdirSource += `      case ${index}:\n`;
+          constructor.arguments.forEach((argument, argumentIndex) => {
+            fdirSource += `        const arg${argumentIndex} = ${generateValue(argument.idlType.idlType)};\n`;
+          });
+          fdirSource += `        this._parameters = [${constructor.arguments.map((_, argumentIndex) => `arg${argumentIndex}`).join(", ")}];\n`;
+          fdirSource += `        break;\n`;
+        });
+
+        fdirSource += `    }\n`;
+      } else {
+        fdirSource += `    this._parameters = [];\n`;
+      }
+
+      fdirSource += `  }\n\n`;
+      fdirSource += `  mutate(ctx: GlobalContext): void {\n`;
+      fdirSource += `    this.generate(ctx);\n`;
+      fdirSource += `  }\n\n`;
+      fdirSource += `  lower(): string {\n`;
+
+      if (constructors.length > 0) {
+        fdirSource += `    return \`const \${this._id} = new ${className}(\${this._parameters.join(", ")});\`;\n`;
+      } else {
+        fdirSource += `    return \`const \${this._id} = new ${className}();\`;\n`;
+      }
+
+      fdirSource += `  }\n`;
+      fdirSource += `}\n\nexport {${className}Value};\n\n`;
+    }
+  }
+
+  return fdirSource;
+}
+
+const fdirSource = generateFDIR(parsedWebIDL);
+fs.writeFileSync('FD-IR.ts', fdirSource, 'utf-8');
