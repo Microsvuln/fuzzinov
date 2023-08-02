4839442debd4fcb8494da2d4b0050e546beb22ea
Microsvuln <arash.vre@gmail.com>
2023-08-02 19:15:52 +0200

    remove wrong files

--- mutate-usage.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mutator_1 = require("./mutator");
-// Path to the TypeScript file to fuzz
-var filePath = './file.ts';
-// Apply fuzzing
-(0, mutator_1.fuzzFile)(filePath);
--- mutate-usage.ts
+++
@@ -1,7 +0,0 @@
-import { fuzzFile } from './mutator';
-
-// Path to the TypeScript file to fuzz
-const filePath = './file.ts';
-
-// Apply fuzzing
-fuzzFile(filePath);
